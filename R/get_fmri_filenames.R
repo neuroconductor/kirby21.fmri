26533547bb3ea3d86f41b4fa79d2c1745ad9b823
muschellij2 <muschellij2@gmail.com>
2016-10-04 23:53:16 -0400

    adding files to repo

---
+++ R/get_fmri_filenames.R
@@ -0,0 +1,15 @@
+#' @title Get fMRI Image Filenames
+#'
+#' @description Return the filenames for the fMRI images
+#' @param ... arguments to pass to \code{\link{get_image_filenames}},
+#' \code{modalities = "fMRI"} so it cannot be specified
+#' @return Vector fo filenames
+#' 
+#' @examples
+#' get_fmri_filenames()
+#' @export
+#' @importFrom kirby21.base get_image_filenames
+get_fmri_filenames = function(...) {
+  x = kirby21.base::get_image_filenames(modalities = "fMRI", ...)
+  return(x)
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Structural fMRI magnetic resonance imaging ('MRI')
+data from the 'Kirby21' reproducibility study.
+
+[![Travis-CI Build Status](https://travis-ci.org/muschellij2/kirby21.fmri.svg?branch=master)](https://travis-ci.org/)
+
